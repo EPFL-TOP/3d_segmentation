84dea4f0fd104ac569678448862ee58bc16a72b6
Clement Helsens <clement.helsens@epfl.ch>
2025-03-06 21:18:54 +0100

    Update 3d_cellpose.py

--- 3d_cellpose.py
+++ 3d_cellpose.py
@@ -94,7 +94,7 @@ def main():
             masks,
             flows,
             #str(output_file_base),
-            "/scratch/output/feyza/test1",
+            ["/scratch/output/feyza/test1"],
             channels=channels,
             png=False,  # Save masks as PNGs
             tif=True,  # Save masks as TIFFs
